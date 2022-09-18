efcb8c61dac71fb01c58ef48f92e295211b089a3
Chad <li17206@163.com>
2022-09-18 12:12:54 +0800

    add nginx logs volume

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
+mkdir -p data logs
+
 docker run -d \
         -p 80:80 \
 	--user 501:20 \
         -v `pwd`/conf/:/etc/nginx/:ro \
         -v `pwd`/data/:/opt/nginx/:rw \
+	-v `pwd`/logs/:/var/log/nginx/:rw \
 	-v /etc/localtime:/etc/localtime:ro \
         --name nginx chad/nginx-forward
 
